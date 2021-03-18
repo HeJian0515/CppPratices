10bcfc7be2f78722dfa76ce00eb25efd34c3d4d4
HeJian0515 <2972624919@qq.com>
2021-03-18 10:50:58 +0800

    _20210318

---
+++ include/strand.hpp
@@ -0,0 +1,13 @@
+//! 串问题===============================================================
+#include <string>
+#include <vector>
+
+using namespace std;
+
+namespace _5longestPalinddrome
+{
+string longestPalindrome(string s) 
+{
+    
+}
+}
