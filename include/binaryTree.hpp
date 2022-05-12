eada508398fe99b41907a26324d005f25522d61d
HeJian0515 <2972624919@qq.com>
2022-05-12 21:41:54 +0800

    二叉树提交

--- include/binaryTree.hpp
+++ include/binaryTree.hpp
@@ -412,6 +412,30 @@ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
 }
 
 }
+
+namespace _1 {
+
+    TreeNode* find(TreeNode* root, int val1, int val2)
+    {
+        if (root == nullptr) return nullptr;
+
+        if (root->val == val1 || root->val == val2) {
+            return root;
+        }
+
+        auto left = find(root->left, val1, val2);
+        auto right = find(root->right, val1, val2);
+
+        if (left != nullptr && right != nullptr) return root;
+        return left != nullptr ? left : right;
+    }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+    {
+        return find(root, p->val, q->val);
+    }
+}
+
 }
 
 
