a75a2f3e4a6bedf30a739f0bdbda9406c299220b
HeJian0515 <2972624919@qq.com>
2025-05-21 17:06:06 +0800

    fix: 修复clion报错

--- main.cpp
+++ main.cpp
@@ -1,26 +1,9 @@
-#include <fmt/printf.h>
-#include <gsl/pointers>
-#include <memory>
+#include <iostream>
+#include <algorithm>
 
-struct Foo {
-   static int count;
-
-   int id;
-   explicit Foo(): id(++count) {
-      fmt::print("Foo:{} created\n", id);
-   }
-   ~Foo() {
-      fmt::print("Foo:{} destroyed\n", id);
-   }
-};
-
-int Foo::count = 0;
+using namespace std;
 
 int main() {
-   fmt::print("Hello, {}!\n", "Clion");
-   gsl::owner<Foo*> foo = new Foo();
-   delete foo;
-   gsl::unique_ptr<Foo> foo1 = std::make_unique<Foo>();
-   gsl::unique_ptr<Foo> foo2 = std::make_unique<Foo>();
-   return 0;
+    cout << "Hello World!" << endl;
+    return 0;
 }
