2f066b951527155bec2142bf422ca94d43b409fe
HeJian0515 <2972624919@qq.com>
2025-05-21 16:46:21 +0800

    fix: 去除多余的第三方库

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+#include <fmt/printf.h>
+#include <gsl/pointers>
+#include <memory>
 
+struct Foo {
+   static int count;
+
+   int id;
+   explicit Foo(): id(++count) {
+      fmt::print("Foo:{} created\n", id);
+   }
+   ~Foo() {
+      fmt::print("Foo:{} destroyed\n", id);
+   }
+};
+
+int Foo::count = 0;
 
 int main() {
-    cout << "hello clion" << endl;
+   fmt::print("Hello, {}!\n", "Clion");
+   gsl::owner<Foo*> foo = new Foo();
+   delete foo;
+   gsl::unique_ptr<Foo> foo1 = std::make_unique<Foo>();
+   gsl::unique_ptr<Foo> foo2 = std::make_unique<Foo>();
+   return 0;
 }
